57b3fccd9f5abd526442ce1f113126f452f00b5a
kumpulanremaja <47444671+kumpulanremaja@users.noreply.github.com>
2020-03-28 00:53:09 +0700

    Update vpnsssh.sh

--- vpnsssh.sh
+++ vpnsssh.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 
+
 # install badvpn
 cd
-wget -O /usr/bin/badvpn-udpgw "http://satria.asia/repo/badvpn-udpgw"
+wget -O /usr/bin/badvpn-udpgw "https://raw.github.com/arieonline/autoscript/master/conf/badvpn-udpgw"
+if [ "$OS" == "x86_64" ]; then
+  wget -O /usr/bin/badvpn-udpgw "https://raw.github.com/arieonline/autoscript/master/conf/badvpn-udpgw64"
+fi
 sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.d/rc.local
 chmod +x /usr/bin/badvpn-udpgw
 screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
+
