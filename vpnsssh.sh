37060ae8359fe4c2791ae8d26d04c8089950df4d
kumpulanremaja <47444671+kumpulanremaja@users.noreply.github.com>
2020-03-28 00:45:41 +0700

    Create vpnsssh.sh

---
+++ vpnsssh.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+# install badvpn
+cd
+wget -O /usr/bin/badvpn-udpgw "http://satria.asia/repo/badvpn-udpgw"
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
+chmod +x /usr/bin/badvpn-udpgw
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
